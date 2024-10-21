fe0cbe29f645a997c06c5622b10025ba74fed34b
Wei1024 <weihua.huang24@gmail.com>
2024-10-21 13:49:25 -0700

    moved download functions to utilities

--- app.py
+++ app.py
@@ -7,37 +7,7 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 from urllib.parse import urljoin
 from source.reader import process_question
-
-# Function to download a PDF file
-def download_pdf(url, destination_folder):
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        filename = os.path.join(destination_folder, url.split("/")[-1])
-        with open(filename, 'wb') as f:
-            response.raw.decode_content = True
-            shutil.copyfileobj(response.raw, f)
-        return filename
-    else:
-        return None
-
-def download_from_cfm(url, destination_folder):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        # Assuming that the PDF link is inside <a> tags
-        pdf_links = soup.find_all('a', href=True)
-        downloaded_files = []
-        for link in pdf_links:
-            href = link['href']
-            if href.endswith(".pdf"):
-                # Use urljoin to handle relative URLs correctly
-                pdf_url = urljoin(url, href)
-                pdf_filename = download_pdf(pdf_url, destination_folder)
-                if pdf_filename:
-                    downloaded_files.append(pdf_filename)
-        return downloaded_files
-    return None
-
+from source.utilities import download_pdf, download_from_cfm
 
 # Create a temporary directory
 tmp_dir = Path("tmp")
